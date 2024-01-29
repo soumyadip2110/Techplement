8df8ec095951701b7ca2f35d8bb4051e45729842
Soumyadip Chowdhury <soumyadip2110@gmail.com>
2024-01-29 23:53:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const skills = document.getElementById('skills');
+const education = document.getElementById('education');
+const experience = document.getElementById('experience');
+
+skills.addEventListener('click', () => {
+    document.querySelector('.skills-tab').classList.toggle('display-tab');
+    document.querySelector('.education-tab').classList.remove('display-tab');
+    document.querySelector('.experience-tab').classList.remove('display-tab');
+});
+
+education.addEventListener('click', () => {
+    document.querySelector('.education-tab').classList.toggle('display-tab');
+    document.querySelector('.skills-tab').classList.remove('display-tab');
+    document.querySelector('.experience-tab').classList.remove('display-tab');
+})
+experience.addEventListener('click', () => {
+    document.querySelector('.experience-tab').classList.toggle('display-tab');
+    document.querySelector('.skills-tab').classList.remove('display-tab');
+    document.querySelector('.education-tab').classList.remove('display-tab');
+})
