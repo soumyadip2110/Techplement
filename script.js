a5fc7d2fe836338f5a35db3c0cb2cdbd8f9b320f
Soumyadip Chowdhury <soumyadip2110@gmail.com>
2024-01-30 18:37:49 +0530

    fix responsiveness

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const skills = document.getElementById('skills');
 const education = document.getElementById('education');
 const experience = document.getElementById('experience');
+const menuBtn = document.querySelector('.mobile-nav-btn');
 
 skills.addEventListener('click', () => {
     document.querySelector('.skills-tab').classList.toggle('display-tab');
@@ -12,9 +13,13 @@ education.addEventListener('click', () => {
     document.querySelector('.education-tab').classList.toggle('display-tab');
     document.querySelector('.skills-tab').classList.remove('display-tab');
     document.querySelector('.experience-tab').classList.remove('display-tab');
-})
+});
 experience.addEventListener('click', () => {
     document.querySelector('.experience-tab').classList.toggle('display-tab');
     document.querySelector('.skills-tab').classList.remove('display-tab');
     document.querySelector('.education-tab').classList.remove('display-tab');
-})
+});
+
+menuBtn.addEventListener('click', () => {
+    document.querySelector('.mobile-nav').classList.toggle('active');
+});
